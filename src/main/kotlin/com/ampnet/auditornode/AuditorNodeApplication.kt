7ab212294ff8018a8bc54c980537623d1b944b5c
Domagoj Latečki <domagoj.latecki@gmail.com>
2021-04-29 17:13:10 +0200

    SFDEV-8: refactor RPC connectors, generate hash storage transaction

--- src/main/kotlin/com/ampnet/auditornode/AuditorNodeApplication.kt
+++ src/main/kotlin/com/ampnet/auditornode/AuditorNodeApplication.kt
@@ -1,56 +1,35 @@
 package com.ampnet.auditornode
 
 import arrow.core.computations.either
-import com.ampnet.auditornode.contract.ExampleStorageContractRPCConnector
+import com.ampnet.auditornode.contract.ContractAbi
 import com.ampnet.auditornode.error.ApplicationError
 import com.ampnet.auditornode.ipfs.GatewayIpfsClient
 import com.ampnet.auditornode.ipfs.LocalIpfsClient
-import com.ampnet.auditornode.script.api.AuditResult
+import com.ampnet.auditornode.rpc.RpcClient
 import com.ampnet.auditornode.script.evaluation.JavaScriptEvaluator
-import org.kethereum.model.Address
-import org.kethereum.rpc.HttpEthereumRPC
-import org.web3j.protocol.Web3j
-import org.web3j.protocol.http.HttpService
-import java.net.ConnectException
+import org.kethereum.model.Transaction
 
 const val INFURA_RPC_URL = "https://ropsten.infura.io/v3/08664baf7af14eda956db2b71a79f12f"
 const val LOCAL_RPC_URL = "http://localhost:8545"
-const val CONTRACT_ADDRESS = "0x7FE38DAeeE945c53AC6CbbeD81c4e3cf7FF88Ac0"
 
 suspend fun main(args: Array<String>) {
     val rpcBaseUrl = if (args.contains("--local-geth")) LOCAL_RPC_URL else INFURA_RPC_URL
     println("RPC base URL: $rpcBaseUrl")
 
-    val web3 = Web3j.build(HttpService(rpcBaseUrl))
-
-    try {
-        val currentBlockNumber = web3.ethBlockNumber().send().blockNumber
-        println("Current block number: $currentBlockNumber")
-    } catch (e: ConnectException) {
-        println("Cannot connect to RPC: $rpcBaseUrl")
-        throw e
-    }
-
-    val ipfsFileHash = ExampleStorageContractRPCConnector(
-        Address(CONTRACT_ADDRESS),
-        HttpEthereumRPC(baseURL = rpcBaseUrl)
-    )
-        .hash()
-
-    requireNotNull(ipfsFileHash) {
-        "Could not retrieve IPFS file hash; make sure your local ethereum light client is " +
-            "fully synced with Ropsten testnet"
-    }
-
-    println("Input IPFS hash: $ipfsFileHash")
-
     val ipfsClient = if (args.contains("--local-ipfs")) LocalIpfsClient else GatewayIpfsClient
     println("Using ${ipfsClient.javaClass.simpleName}")
 
-    val result = either<ApplicationError, AuditResult> {
+    val contractAbi = ContractAbi(rpcBaseUrl)
+    val rpcClient = RpcClient(rpcBaseUrl)
+
+    val result = either<ApplicationError, Transaction> {
+        val currentBlockNumber = rpcClient.currentBlockNumber().bind()
+        println("Current block number: $currentBlockNumber")
+        val ipfsFileHash = contractAbi.getIpfsFileHash().bind()
+        println("Input IPFS hash: $ipfsFileHash")
         val ipfsFile = ipfsClient.fetchFile(ipfsFileHash).bind()
         val evaluationResult = JavaScriptEvaluator.evaluate(ipfsFile).bind()
-        evaluationResult
+        contractAbi.storeIpfsFileHash("${evaluationResult.success}")
     }
 
     println(result)
---
+++ src/main/kotlin/com/ampnet/auditornode/rpc/RpcClient.kt
@@ -0,0 +1,20 @@
+package com.ampnet.auditornode.rpc
+
+import arrow.core.Either
+import com.ampnet.auditornode.error.RpcError
+import com.ampnet.auditornode.error.Try
+import org.web3j.protocol.Web3j
+import org.web3j.protocol.http.HttpService
+import java.math.BigInteger
+
+class RpcClient(private val rpcBaseUrl: String) {
+
+    private val web3 = Web3j.build(HttpService(rpcBaseUrl))
+
+    fun currentBlockNumber(): Try<BigInteger> =
+        Either.catch {
+            web3.ethBlockNumber().send().blockNumber
+        }
+            .mapLeft { RpcError.RpcConnectionError(rpcBaseUrl, it) }
+
+}
