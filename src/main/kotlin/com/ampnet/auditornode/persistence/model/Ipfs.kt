a5fc665427c98eff1349e4153e573770b5090bfd
Domagoj Latečki <domagoj.latecki@gmail.com>
2021-05-14 15:52:27 +0200

    SD-73: minor code style fixes

--- src/main/kotlin/com/ampnet/auditornode/persistence/model/Ipfs.kt
+++ src/main/kotlin/com/ampnet/auditornode/persistence/model/Ipfs.kt
@@ -3,4 +3,3 @@ package com.ampnet.auditornode.persistence.model
 inline class IpfsHash(val value: String) // TODO use `value class` instead on later Kotlin version
 
 inline class IpfsTextFile(val content: String) // TODO use `value class` instead on later Kotlin version
-
--- src/main/kotlin/com/ampnet/auditornode/persistence/model/Script.kt
+++ src/main/kotlin/com/ampnet/auditornode/persistence/model/Script.kt
@@ -4,4 +4,4 @@ import java.util.UUID
 
 inline class ScriptSource(val content: String) // TODO use `value class` instead on later Kotlin version
 
-inline class ScriptId(val value: UUID)  // TODO use `value class` instead on later Kotlin version
+inline class ScriptId(val value: UUID) // TODO use `value class` instead on later Kotlin version
