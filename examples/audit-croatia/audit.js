03b1f18624ec69ac31c4ff26741db0d5de4b227e
Domagoj Latečki <domagoj.latecki@gmail.com>
2021-05-24 17:26:29 +0200

    SD-101: write example auditing script for Croatia

---
+++ examples/audit-croatia/audit.js
@@ -0,0 +1,78 @@
+function abortAudit(message) {
+    Output.renderText("Please try again by following the instructions in the script.");
+    return AuditResult.aborted(message);
+}
+
+function abortOrInvalidateAudit(message) {
+    if (Input.readBoolean("Should the audit of this asset be marked as failed?")) {
+        return AuditResult.failure(message);
+    }
+
+    Output.renderText("Please try again by following the instructions in the script.");
+    return AuditResult.aborted(message);
+}
+
+function audit(auditData) {
+    Output.renderHtml(Ipfs.getFile("step-1.html"));
+    Input.button("Continue");
+    Output.renderHtml(Ipfs.getFile("step-2.html"));
+
+    if (!Input.readBoolean("Is everything as described above?")) {
+        return abortAudit("Audit aborted before validation");
+    }
+
+    Output.renderHtml(Ipfs.getFile("step-3.html"));
+
+    if (!Input.readBoolean("Does the search prompt look like described above?")) {
+        return abortAudit("Audit aborted before cadastral office was found");
+    }
+
+    Output.renderHtml(
+        Ipfs.getFile("step-4.html")
+            .replace(/{cadastralOffice}/g, auditData.assetInfo.cadastralOffice)
+    );
+
+    if (!Input.readBoolean("Did you manage to find the requested value?")) {
+        return abortOrInvalidateAudit("Cadastral office not found");
+    }
+
+    Output.renderHtml(Ipfs.getFile("step-5.html"));
+
+    if (!Input.readBoolean("Does the search prompt look like described above?")) {
+        return abortAudit("Audit aborted before municipality was found");
+    }
+
+    Output.renderHtml(
+        Ipfs.getFile("step-6.html")
+            .replace(/{cadastralMunicipalityName}/g, auditData.assetInfo.cadastralMunicipalityName)
+            .replace(/{cadastralMunicipalityNumber}/g, auditData.assetInfo.cadastralMunicipalityNumber)
+    );
+
+    if (!Input.readBoolean("Did you manage to find the requested value?")) {
+        return abortOrInvalidateAudit("Cadastral municipality not found");
+    }
+
+    Output.renderHtml(
+        Ipfs.getFile("step-7.html")
+            .replace(/{parcelNumber}/g, auditData.assetInfo.parcelNumber)
+            .replace(/{parcelSubNumber}/g, auditData.assetInfo.parcelSubNumber)
+    );
+
+    if (!Input.readBoolean("Does the first table below the input fields show any values after passing the captcha?")) {
+        return abortOrInvalidateAudit("Asset info not found");
+    }
+
+    Output.renderHtml(Ipfs.getFile("step-8.html"));
+
+    if (!Input.readBoolean("Do you see the document after clicking on the button?")) {
+        return abortOrInvalidateAudit("Ownership document not found");
+    }
+
+    Output.renderHtml(Ipfs.getFile("step-9.html"));
+
+    if (!Input.readBoolean("Do the document contain the required data?")) {
+        return abortOrInvalidateAudit("Ownership not confirmed");
+    }
+
+    return AuditResult.success();
+}
