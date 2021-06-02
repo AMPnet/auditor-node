13436c8c576bb8eb44081e2c0175670e9f6c8097
Domagoj Latečki <domagoj.latecki@gmail.com>
2021-06-02 16:19:42 +0200

    SD-113: refactor build script

---
+++ buildSrc/src/main/kotlin/Configurations.kt
@@ -0,0 +1,28 @@
+import java.math.BigDecimal
+
+object Configurations {
+
+    object Compile {
+        val compilerArgs = listOf("-Xjsr305=strict")
+    }
+
+    object Docker {
+        const val graalBaseImage = "ghcr.io/graalvm/graalvm-ce"
+        const val graalTag = "ol8-java8-${Versions.Dependencies.graalSdk}"
+        const val graalDigest = "sha256:2754d08ca9ca494d6947f214d66e02ab7bd02192ee13ed9e2f5c802d588040e0"
+        const val nativeBaseImage = "gcr.io/distroless/cc-debian10"
+        const val nativeDigest = "sha256:4cad7484b00d98ecb300916b1ab71d6c71babd6860c6c5dd6313be41a8c55adb"
+    }
+
+    object NativeImage {
+        val args = listOf(
+            "--initialize-at-build-time",
+            "--language:js"
+        )
+    }
+
+    object Tests {
+        val testSets = listOf("integTest", "apiTest")
+        val minimumCoverage = BigDecimal("0.7")
+    }
+}
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,43 @@
+import org.gradle.api.JavaVersion
+
+object Versions {
+
+    const val project = "0.0.1"
+
+    object Compile {
+        const val kotlin = "1.5.0"
+        val sourceCompatibility = JavaVersion.VERSION_1_8
+        val targetCompatibility = JavaVersion.VERSION_1_8
+        val jvmTarget = targetCompatibility.name.removePrefix("VERSION_").replace('_', '.')
+    }
+
+    object Plugins {
+        const val kethabi = "0.2.0"
+        const val allOpen = Compile.kotlin
+        const val ktlint = "10.0.0"
+        const val detekt = "1.17.1"
+        const val testSets = "4.0.0"
+        const val testLogger = "3.0.0"
+        const val micronaut = "1.5.0"
+        const val shadowJar = "6.1.0"
+    }
+
+    object Tools {
+        const val ktlint = "0.41.0"
+        const val micronaut = "2.5.4"
+        const val jacoco = "0.8.7"
+    }
+
+    object Dependencies {
+        const val okHttp = "4.9.1"
+        const val graalSdk = "21.0.0.2"
+        const val web3jCore = "5.0.0"
+        const val arrowCore = "0.13.2"
+        const val kotlinCoroutines = Compile.kotlin
+        const val kotlinLogging = "2.0.6"
+        const val mockitoKotlin = "3.2.0"
+        const val assertk = "0.24"
+        const val micronautTestCore = "2.3.6"
+        const val wireMock = "2.27.2"
+    }
+}
